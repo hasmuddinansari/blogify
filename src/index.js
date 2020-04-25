e7e6ebad6b1628b9b0ce62acee2a376f54d70394
Hasmuddin <mdaliansari33@gmail.com>
2020-04-25 14:01:48 +0530

    pwa

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import store from "./Redux/store"
 
-ReactDOM.hydrate(
+ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>
@@ -16,4 +16,4 @@ ReactDOM.hydrate(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
